b72b1df1ddc161a7f739b8cabc92a78486253136
Clisika <clisika@myges.fr>
2025-01-13 00:14:08 +0100

    add: window folder that manage initiation and destroy of window

---
+++ src/window/init_window.c
@@ -0,0 +1,30 @@
+/* ********************************************************************************************************* */
+/*                                                                                                           */
+/*                                                              :::::::::: ::::::::   :::::::: :::::::::::   */
+/*   init_window.c                                             :+:       :+:    :+: :+:    :+:    :+:        */
+/*                                                            +:+       +:+        +:+           +:+         */
+/*   By: lisika <lisika@myges.fr>                            +#++:++#  +#++:++#++ :#:           +#+          */
+/*                                                          +#+              +#+ +#+   +#+#    +#+           */
+/*   Created: 2025/01/12 22:48:43 by lisika                #+#       #+#    #+# #+#    #+#    #+#            */
+/*   Updated: 2025/01/12 22:48:43 by lisika               ########## ########   ######## ###########         */
+/*                                                                                                           */
+/* ********************************************************************************************************* */
+
+#include "include.h"
+#include "macros.h"
+#include "struct.h"
+#include "prototype.h"
+
+window_t *init_window(window_t *window)
+{
+    window = malloc(sizeof(window_t));
+    if (window == NULL)
+        return NULL;
+    window->VideoMode = sfVideoMode_getDesktopMode();
+    window->Window = sfRenderWindow_create(window->VideoMode, WINDOW_NAME,
+    sfClose | sfResize, NULL);
+    window->windowSize = sfRenderWindow_getSize(window->Window);
+    window->mousePos = sfMouse_getPositionRenderWindow(window->Window);
+    window->statePage = HOME_PAGE;
+    return window;
+}
